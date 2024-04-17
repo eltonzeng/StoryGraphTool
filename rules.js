51111be07ffb4f61205f4ff310d028497c0b47c8
Elton Zeng <ezeng2@ucsc.edu>
2024-04-16 18:22:54 -0700

    "updated myStory.json and rules.js"

--- rules.js
+++ rules.js
@@ -1,23 +1,30 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
+        console.log(this.engine.storyData);
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
+let flag = 0;
+
 class Location extends Scene {
+
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        if (this.myKeys == undefined) {
+            this.myKeys = {};
+        }
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        if(locationData.Choices != null) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                // TODO: use the Text of the choice
+                this.engine.addChoice(choice.Text, choice); // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
             this.engine.addChoice("The end.")
@@ -25,12 +32,53 @@ class Location extends Scene {
     }
 
     handleChoice(choice) {
+        console.log(flag);
         if(choice) {
+            if (choice.Key) {
+                this.myKeys[choice.Key] = true;
+                this.engine.show(choice.KeyText);
+            }
+            if (choice.NeedsKey) {
+                let key_text = choice.NeedsKey;
+                if (this.myKeys[key_text]) {
+                    this.engine.gotoScene(Location, choice.Target)
+                } else {
+                    this.engine.show(choice.LockedMessage);
+                }
+            }
+            // if choice.key is not null (i.e., I have a Key property) then
+            //    set a global variable that I have this key myKeys[keyname] = true
+            //    display the key text from choice.KeyText
+            // if choice.NeedsKey then
+            //     get the name of the required key from needsKey
+            //    if myKeys[required keyname] is true then
+            //        go to new location
+            //    else
+            //        display locked message from choice.lockedmessage
+            if (choice.Action === "eatTomatoEgg") {
+                this.eatTomatoEgg();
+                this.engine.gotoScene(Location, choice.Target);
+            } else if (choice.locationData === "Garden Creamery" && flag === 1) {
+                this.engine.show(locationData.Unlocked_Message);
+            } else if (choice.locationData === "Garden Creamery" && flag != 1) {
+                this.engine.show(locationData.Locked_Message);
+            }else {
             this.engine.show("&gt; "+choice.Text);
             this.engine.gotoScene(Location, choice.Target);
+            }
         } else {
             this.engine.gotoScene(End);
         }
+    }
+
+    eatTomatoEgg() {
+        this.engine.show("Rest your labours, Reflect on your heritage, and Remind yourself what really matters.");
+    }
+
+    talkToShawn(choice) {
+        this.engine.show("You talk to Shawn and he gives you a key.");
+        flag = 1;
+        this.engine.gotoScene(Location, this.engine.currentSceneKey);
     }
 }
 
